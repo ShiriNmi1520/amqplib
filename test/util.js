c5f54ffd979844702079164ddb534a51fa122e5b
Stephen Cresswell <229672+cressie176@users.noreply.github.com>
2025-09-11 16:32:26 +0100

    Refactor utility functions out of codec tests

--- test/util.js
+++ test/util.js
@@ -157,15 +157,92 @@ function kCallback(k, ek) {
   };
 }
 
+// When encoding, you can supply explicitly-typed fields like `{'!':
+// int32, 50}`. Most of these do not appear in the decoded values, so
+// to compare like-to-like we have to remove them from the input.
+function removeExplicitTypes(input) {
+  switch (typeof input) {
+    case 'object':
+      if (input == null) {
+        return null;
+      }
+      if (Array.isArray(input)) {
+        const newArr = [];
+        for (let i = 0; i < input.length; i++) {
+          newArr[i] = removeExplicitTypes(input[i]);
+        }
+        return newArr;
+      }
+      if (Buffer.isBuffer(input)) {
+        return input;
+      }
+      switch (input['!']) {
+        case 'timestamp':
+        case 'decimal':
+        case 'float':
+          return input;
+        case undefined: {
+          const newObj = {};
+          for (const k in input) {
+            newObj[k] = removeExplicitTypes(input[k]);
+          }
+          return newObj;
+        }
+        default:
+          return input.value;
+      }
+
+    default:
+      return input;
+  }
+}
+
+// Asserts that the decoded fields are equal to the original fields,
+// or equal to a default where absent in the original. The defaults
+// depend on the type of method or properties.
+//
+// This works slightly different for methods and properties: for
+// methods, each field must have a value, so the default is
+// substituted for undefined values when encoding; for properties,
+// fields may be absent in the encoded value, so a default is
+// substituted for missing fields when decoding. The effect is the
+// same so far as these tests are concerned.
+function assertEqualModuloDefaults(original, decodedFields) {
+  const args = defs.info(original.id).args;
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    const originalValue = original.fields[arg.name];
+    const decodedValue = decodedFields[arg.name];
+    try {
+      if (originalValue === undefined) {
+        // longstr gets special treatment here, since the defaults are
+        // given as strings rather than buffers, but the decoded values
+        // will be buffers.
+        assert.deepEqual(arg.type === 'longstr' ? Buffer.from(arg.default) : arg.default, decodedValue);
+      } else {
+        assert.deepEqual(removeExplicitTypes(originalValue), decodedValue);
+      }
+    } catch (assertionErr) {
+      const methodOrProps = defs.info(original.id).name;
+      assertionErr.message += ` (frame ${methodOrProps} field ${arg.name})`;
+      throw assertionErr;
+    }
+  }
+  // %%% TODO make sure there's no surplus fields
+  return true;
+}
+
 module.exports = {
-  socketPair: socketPair,
-  runServer: runServer,
-  succeed: succeed,
-  succeedIfAttributeEquals: succeedIfAttributeEquals,
-  fail: fail,
-  latch: latch,
-  completes: completes,
-  kCallback: kCallback,
-  schedule: schedule,
-  randomString: randomString,
+  socketPair,
+  runServer,
+  succeed,
+  succeedIfAttributeEquals,
+  fail,
+  latch,
+  completes,
+  kCallback,
+  schedule,
+  randomString,
+  removeExplicitTypes,
+  assertEqualModuloDefaults,
 };
